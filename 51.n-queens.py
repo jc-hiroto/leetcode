1dac856fba7471b69c418479730645cb9fd823af
jc-hiroto <taste.ful.msa@gmail.com>
2022-06-04 18:31:23 +0800

    0604 daily challenge and more

---
+++ 51.n-queens.py
@@ -0,0 +1,68 @@
+#
+# @lc app=leetcode id=51 lang=python3
+#
+# [51] N-Queens
+#
+# https://leetcode.com/problems/n-queens/description/
+#
+# algorithms
+# Hard (57.38%)
+# Likes:    6476
+# Dislikes: 162
+# Total Accepted:    391.1K
+# Total Submissions: 662.8K
+# Testcase Example:  '4'
+#
+# The n-queens puzzle is the problem of placing n queens on an n x n chessboard
+# such that no two queens attack each other.
+# 
+# Given an integer n, return all distinct solutions to the n-queens puzzle. You
+# may return the answer in any order.
+# 
+# Each solution contains a distinct board configuration of the n-queens'
+# placement, where 'Q' and '.' both indicate a queen and an empty space,
+# respectively.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: n = 4
+# Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+# Explanation: There exist two distinct solutions to the 4-queens puzzle as
+# shown above
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: n = 1
+# Output: [["Q"]]
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= n <= 9
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        def dfs(queens, xy_diff, xy_sum):
+            p = len(queens)
+            if p == n:
+                res.append(queens)
+                return None
+            for q in range(n):
+                if q not in queens and p-q not in xy_diff and p+q not in xy_sum:
+                    dfs(queens+[q], xy_diff+[p-q], xy_sum+[p+q])
+        res = []
+        dfs([],[],[])
+        return [["." * i + "Q" + "."*(n-i-1) for i in pair] for pair in res]
+        
+# @lc code=end
+
