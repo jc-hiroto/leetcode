fad13eb22a82e56bf4e6566c3351d2be5a146b06
jc-hiroto <taste.ful.msa@gmail.com>
2022-06-12 22:04:09 +0800

    0612 google questions

---
+++ 1525.number-of-good-ways-to-split-a-string.py
@@ -0,0 +1,83 @@
+#
+# @lc app=leetcode id=1525 lang=python3
+#
+# [1525] Number of Good Ways to Split a String
+#
+# https://leetcode.com/problems/number-of-good-ways-to-split-a-string/description/
+#
+# algorithms
+# Medium (70.11%)
+# Likes:    1386
+# Dislikes: 32
+# Total Accepted:    72.7K
+# Total Submissions: 104.1K
+# Testcase Example:  '"aacaba"'
+#
+# You are given a string s.
+# 
+# A split is called good if you can split s into two non-empty strings sleft
+# and sright where their concatenation is equal to s (i.e., sleft + sright = s)
+# and the number of distinct letters in sleft and sright is the same.
+# 
+# Return the number of good splits you can make in s.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: s = "aacaba"
+# Output: 2
+# Explanation: There are 5 ways to split "aacaba" and 2 of them are good. 
+# ("a", "acaba") Left string and right string contains 1 and 3 different
+# letters respectively.
+# ("aa", "caba") Left string and right string contains 1 and 3 different
+# letters respectively.
+# ("aac", "aba") Left string and right string contains 2 and 2 different
+# letters respectively (good split).
+# ("aaca", "ba") Left string and right string contains 2 and 2 different
+# letters respectively (good split).
+# ("aacab", "a") Left string and right string contains 3 and 1 different
+# letters respectively.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: s = "abcd"
+# Output: 1
+# Explanation: Split the string as follows ("ab", "cd").
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= s.length <= 10^5
+# s consists of only lowercase English letters.
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def numSplits(self, s: str) -> int:
+        helper = set([])
+        prev = [0] * len(s)
+        suff = [0] * len(s)
+        for i in range(len(s)):
+            helper.add(s[i])
+            prev[i] = len(helper)
+        helper = set([])
+        for i in range(len(s)-1, -1, -1):
+            helper.add(s[i])
+            suff[i] = len(helper)
+        # print(prev)
+        # print(suff)
+        cnt = 0
+        for i in range(1, len(s)):
+            cnt += prev[i-1] == suff[i]
+        return cnt
+
+        
+# @lc code=end
+
