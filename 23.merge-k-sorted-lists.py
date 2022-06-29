6145a5dbab4b862671bd8cc5af1dc5f06ed8bf1e
jc-hiroto <taste.ful.msa@gmail.com>
2022-06-29 20:52:48 +0800

    Some left questions.

---
+++ 23.merge-k-sorted-lists.py
@@ -0,0 +1,91 @@
+#
+# @lc app=leetcode id=23 lang=python3
+#
+# [23] Merge k Sorted Lists
+#
+# https://leetcode.com/problems/merge-k-sorted-lists/description/
+#
+# algorithms
+# Hard (47.04%)
+# Likes:    12635
+# Dislikes: 487
+# Total Accepted:    1.3M
+# Total Submissions: 2.7M
+# Testcase Example:  '[[1,4,5],[1,3,4],[2,6]]'
+#
+# You are given an array of k linked-lists lists, each linked-list is sorted in
+# ascending order.
+#
+# Merge all the linked-lists into one sorted linked-list and return it.
+#
+#
+# Example 1:
+#
+#
+# Input: lists = [[1,4,5],[1,3,4],[2,6]]
+# Output: [1,1,2,3,4,4,5,6]
+# Explanation: The linked-lists are:
+# [
+# ⁠ 1->4->5,
+# ⁠ 1->3->4,
+# ⁠ 2->6
+# ]
+# merging them into one sorted list:
+# 1->1->2->3->4->4->5->6
+#
+#
+# Example 2:
+#
+#
+# Input: lists = []
+# Output: []
+#
+#
+# Example 3:
+#
+#
+# Input: lists = [[]]
+# Output: []
+#
+#
+#
+# Constraints:
+#
+#
+# k == lists.length
+# 0 <= k <= 10^4
+# 0 <= lists[i].length <= 500
+# -10^4 <= lists[i][j] <= 10^4
+# lists[i] is sorted in ascending order.
+# The sum of lists[i].length will not exceed 10^4.
+#
+#
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+import heapq
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        res = head = ListNode(0)
+        q = []
+        for node in lists:
+            while node:
+                heapq.heappush(q, node.val)
+                node = node.next
+        while q:
+            res.next = ListNode(heapq.heappop(q))
+            res = res.next
+
+        return head.next
+
+
+# @lc code=end
