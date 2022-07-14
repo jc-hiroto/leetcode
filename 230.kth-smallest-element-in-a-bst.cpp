6f8a884dcf51cb986971cebe5820fa158569fe80
jc-hiroto <taste.ful.msa@gmail.com>
2022-07-14 17:14:27 +0800

    0714 daily challenge and lc75.

---
+++ 230.kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode id=230 lang=cpp
+ *
+ * [230] Kth Smallest Element in a BST
+ *
+ * https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (67.60%)
+ * Likes:    6966
+ * Dislikes: 128
+ * Total Accepted:    790.4K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[3,1,4,null,2]\n1'
+ *
+ * Given the root of a binary search tree, and an integer k, return the k^th
+ * smallest value (1-indexed) of all the values of the nodes in the tree.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [3,1,4,null,2], k = 1
+ * Output: 1
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [5,3,6,2,4,null,null,1], k = 3
+ * Output: 3
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is n.
+ * 1 <= k <= n <= 10^4
+ * 0 <= Node.val <= 10^4
+ *
+ *
+ *
+ * Follow up: If the BST is modified often (i.e., we can do insert and delete
+ * operations) and you need to find the kth smallest frequently, how would you
+ * optimize?
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+   public:
+    int kthSmallest(TreeNode* root, int k) { return inorder(root)[k - 1]; }
+    vector<int> inorder(TreeNode* node) {
+        vector<int> res = {};
+        if (node != nullptr) {
+            vector<int> left = inorder(node->left);
+            vector<int> right = inorder(node->right);
+            res.insert(res.end(), left.begin(), left.end());
+            res.push_back(node->val);
+            res.insert(res.end(), right.begin(), right.end());
+            return res;
+        }
+        return vector<int>();
+    }
+};
+// @lc code=end
