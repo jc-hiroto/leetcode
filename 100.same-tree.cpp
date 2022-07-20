bd4116939343adc1bf068e8f2f83f6b4f5ba6ba9
jc-hiroto <taste.ful.msa@gmail.com>
2022-07-21 01:00:21 +0800

    lc75 100.

---
+++ 100.same-tree.cpp
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode id=100 lang=cpp
+ *
+ * [100] Same Tree
+ *
+ * https://leetcode.com/problems/same-tree/description/
+ *
+ * algorithms
+ * Easy (55.52%)
+ * Likes:    6388
+ * Dislikes: 144
+ * Total Accepted:    1.1M
+ * Total Submissions: 2M
+ * Testcase Example:  '[1,2,3]\n[1,2,3]'
+ *
+ * Given the roots of two binary trees p and q, write a function to check if
+ * they are the same or not.
+ *
+ * Two binary trees are considered the same if they are structurally identical,
+ * and the nodes have the same value.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: p = [1,2,3], q = [1,2,3]
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: p = [1,2], q = [1,null,2]
+ * Output: false
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: p = [1,2,1], q = [1,1,2]
+ * Output: false
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in both trees is in the range [0, 100].
+ * -10^4 <= Node.val <= 10^4
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+   public:
+    bool isSameTree(TreeNode* p, TreeNode* q) { return dfs(p, q); }
+    bool dfs(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+        if (p->val != q->val) return false;
+        return dfs(p->left, q->left) && dfs(p->right, q->right);
+    }
+};
+// @lc code=end
---
+++ 100.same-tree.py
@@ -0,0 +1,74 @@
+#
+# @lc app=leetcode id=100 lang=python3
+#
+# [100] Same Tree
+#
+# https://leetcode.com/problems/same-tree/description/
+#
+# algorithms
+# Easy (55.52%)
+# Likes:    6388
+# Dislikes: 144
+# Total Accepted:    1.1M
+# Total Submissions: 2M
+# Testcase Example:  '[1,2,3]\n[1,2,3]'
+#
+# Given the roots of two binary trees p and q, write a function to check if
+# they are the same or not.
+#
+# Two binary trees are considered the same if they are structurally identical,
+# and the nodes have the same value.
+#
+#
+# Example 1:
+#
+#
+# Input: p = [1,2,3], q = [1,2,3]
+# Output: true
+#
+#
+# Example 2:
+#
+#
+# Input: p = [1,2], q = [1,null,2]
+# Output: false
+#
+#
+# Example 3:
+#
+#
+# Input: p = [1,2,1], q = [1,1,2]
+# Output: false
+#
+#
+#
+# Constraints:
+#
+#
+# The number of nodes in both trees is in the range [0, 100].
+# -10^4 <= Node.val <= 10^4
+#
+#
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        def dfs(p, q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val != q.val:
+                return False
+            return dfs(p.left, q.left) and dfs(p.right, q.right)
+
+        return dfs(p, q)
+# @lc code=end
