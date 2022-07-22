192d48d09d4614625bcb2be2a9e8b8757b5f71c6
jc-hiroto <taste.ful.msa@gmail.com>
2022-07-22 17:31:19 +0800

    lc75 56.

---
+++ 56.merge-intervals.cpp
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode id=56 lang=cpp
+ *
+ * [56] Merge Intervals
+ *
+ * https://leetcode.com/problems/merge-intervals/description/
+ *
+ * algorithms
+ * Medium (44.89%)
+ * Likes:    14138
+ * Dislikes: 534
+ * Total Accepted:    1.5M
+ * Total Submissions: 3.3M
+ * Testcase Example:  '[[1,3],[2,6],[8,10],[15,18]]'
+ *
+ * Given an array of intervals where intervals[i] = [starti, endi], merge all
+ * overlapping intervals, and return an array of the non-overlapping intervals
+ * that cover all the intervals in the input.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+ * Output: [[1,6],[8,10],[15,18]]
+ * Explanation: Since intervals [1,3] and [2,6] overlap, merge them into
+ * [1,6].
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: intervals = [[1,4],[4,5]]
+ * Output: [[1,5]]
+ * Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= intervals.length <= 10^4
+ * intervals[i].length == 2
+ * 0 <= starti <= endi <= 10^4
+ *
+ *
+ */
+
+// @lc code=start
+class Solution {
+   public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        sort(intervals.begin(), intervals.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 return a[0] < b[0];
+             });
+        for (auto interval : intervals) {
+            if (!res.empty() && interval[0] <= res.back()[1]) {
+                res.back()[1] = max(res.back()[1], interval[1]);
+            } else {
+                res.push_back(interval);
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
