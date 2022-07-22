5a9eb899dae43b543255835ac1048413b642867e
jc-hiroto <taste.ful.msa@gmail.com>
2022-07-22 17:58:21 +0800

    lc75 57.

---
+++ 57.insert-interval.py
@@ -0,0 +1,75 @@
+#
+# @lc app=leetcode id=57 lang=python3
+#
+# [57] Insert Interval
+#
+# https://leetcode.com/problems/insert-interval/description/
+#
+# algorithms
+# Medium (37.38%)
+# Likes:    5371
+# Dislikes: 370
+# Total Accepted:    552.3K
+# Total Submissions: 1.5M
+# Testcase Example:  '[[1,3],[6,9]]\n[2,5]'
+#
+# You are given an array of non-overlapping intervals intervals where
+# intervals[i] = [starti, endi] represent the start and the end of the i^th
+# interval and intervals is sorted in ascending order by starti. You are also
+# given an interval newInterval = [start, end] that represents the start and
+# end of another interval.
+#
+# Insert newInterval into intervals such that intervals is still sorted in
+# ascending order by starti and intervals still does not have any overlapping
+# intervals (merge overlapping intervals if necessary).
+#
+# Return intervals after the insertion.
+#
+#
+# Example 1:
+#
+#
+# Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+# Output: [[1,5],[6,9]]
+#
+#
+# Example 2:
+#
+#
+# Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+# Output: [[1,2],[3,10],[12,16]]
+# Explanation: Because the new interval [4,8] overlaps with
+# [3,5],[6,7],[8,10].
+#
+#
+#
+# Constraints:
+#
+#
+# 0 <= intervals.length <= 10^4
+# intervals[i].length == 2
+# 0 <= starti <= endi <= 10^5
+# intervals is sorted by starti in ascending order.
+# newInterval.length == 2
+# 0 <= start <= end <= 10^5
+#
+#
+#
+
+# @lc code=start
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        for interval in intervals:
+            if interval[1] < newInterval[0]:
+                res.append(interval)
+            elif interval[0] > newInterval[1]:
+                res.append(interval)
+            else:
+                newInterval[0] = min(interval[0], newInterval[0])
+                newInterval[1] = max(interval[1], newInterval[1])
+        res.append(newInterval)
+        res.sort()
+        return res
+
+# @lc code=end
